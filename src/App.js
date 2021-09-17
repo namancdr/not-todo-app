4b02b942c4cfaaf64c355773a36e613bc07ba307
Naman Choudhary <namancdr271@gmail.com>
2021-09-17 16:27:01 +0530

    updating the original files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// todo App
+import './App.css'
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let globalId = 0;
+
+const App = () => {
+
+    const [task, setTask] = useState('')
+    const [notTodos, setNotTodos] = useState([])
+
+    function createNotTodo(event){
+        event.preventDefault()
+        setNotTodos(oldNotTodos => {
+            return [...oldNotTodos, {notTodo: task, id: globalId++ }]
+        })
+    }
+
+    function deleteItem(itemID){
+        setNotTodos(oldNotTodos => oldNotTodos.filter(item => item.id !== itemID))
+    }
+
+    return(
+        <div>
+            <h1>React Not-todo App</h1>
+
+            <form onSubmit={createNotTodo}>
+                <input type="text" value={task} onChange={event => {
+                    setTask(() => {
+                        return event.target.value
+                    })
+                }} />
+                <button vlaue="submit">Create Not-todo</button>
+            </form>
+
+            <ul>
+                {
+                    notTodos.map(item => {
+                        return <div key={item.id}>
+                                    <li>{item.notTodo}</li>
+                                    <button onClick={() => deleteItem(item.id)}>delete</button>
+                        </div>
+                    })
+                }
+            </ul>
+        </div>  
+    )
 }
 
 export default App;
